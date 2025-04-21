f0d1f2c7160f46df68c4df1a0009cb74190b14fd
aaurizon <750048+aaurizon@users.noreply.github.com>
2025-04-21 13:00:17 +0200

    Add OllamaChat class for managing conversations
    Introduced the OllamaChat class to handle conversation management, including prompt chaining and message storage. The class supports JSON serialization, integrates tools via FunctionCallingInterface, and provides methods for adding user messages and executing API calls.

---
+++ src/Ollama/OllamaChat.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Ollama;
+
+use Ai\FunctionCallingInterface;
+
+/**
+ * Gère une conversation (prompt chaining), en stockant les messages échangés.
+ */
+class OllamaChat implements \JsonSerializable
+{
+    protected string $model;
+
+    /**
+     * @var OllamaMessage[]
+     */
+    protected array $messages = [];
+
+    /**
+     * @var FunctionCallingInterface[]
+     */
+    protected array $tools = [];
+
+    public function __construct(
+        protected Ollama $ollama,
+        string|null $model = null,
+        FunctionCallingInterface ...$tools
+    )
+    {
+        $this->model = $model ?? $ollama->model;
+        $this->tools = $tools;
+    }
+
+    /**
+     * Ajoute un message côté “user”.
+     */
+    public function message(string $content, string $role = 'user')// : ChatResponseResource
+    {
+        $this->messages[] = new OllamaMessage($role, $content);
+    }
+
+    public function jsonSerialize() : mixed
+    {
+        return [
+            'model'    => $this->model,
+            'messages' => $this->messages,
+            'tools'    => $this->tools,
+        ];
+    }
+
+    public function data()
+    {
+        return json_encode($this, JSON_PRETTY_PRINT) . PHP_EOL;
+    }
+
+    public function dataInfo()
+    {
+        echo $this->data();
+    }
+
+    public function exec()
+    {
+        $json = $this->jsonSerialize();
+        $json['stream'] = false;
+        $data = fetch_json($this->ollama->base_url . '/api/chat', method: 'POST', data: $json);
+
+        $tool_calls = [];
+        foreach ($data->tool_calls ?? [] as $tool_call)
+        {
+            $tool_calls[] = new \Ai\FunctionCall($tool_call->function->name, $tool_call->function->arguments);
+        }
+
+        $this->messages[] = new OllamaMessage($data->message->role, $data->message->content, ...$tool_calls);
+    }
+}
